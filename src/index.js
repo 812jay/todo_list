8bdff0db580321a3bc49ff502556064b6ebe16c3
jay <yongje812@gmail.com>
2025-01-30 21:28:39 +0900

    feat: firebase hosting setting

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCIsm5Y9TJVq2I-QJKo-HIDgBVpcF3RxB4",
+  authDomain: "todo-list-31e6f.firebaseapp.com",
+  projectId: "todo-list-31e6f",
+  storageBucket: "todo-list-31e6f.firebasestorage.app",
+  messagingSenderId: "1055302102164",
+  appId: "1:1055302102164:web:0f7e698516905101e49881"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
